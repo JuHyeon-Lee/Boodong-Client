1d82bb807aeafabd0d98bb235a62f49e91334d38
JuHyeon-Lee <leon6095@gmail.com>
2021-03-10 18:48:19 +0900

    feat: Header 컴포넌트 구현

---
+++ src/components/Header/Header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const style = {};
+
+style.Header = styled.header`
+    display: flex;
+    padding: 10px;
+    background-color: #f1f1f1;
+    margin-bottom: 20px;
+`;
+
+style.User = styled.label`
+    font-size: 20px;
+    font-weight: bold;
+`;
+
+const Header = () => {
+    return (
+        <style.Header>
+            <style.User>인하공인중개사</style.User>
+        </style.Header>
+    )
+};
+
+export default Header;
