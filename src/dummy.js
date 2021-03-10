cff7b5561f4e82949fd98b60c399f4b86798fb03
JuHyeon-Lee <leon6095@gmail.com>
2021-03-10 18:59:25 +0900

    feat: 더미 데이터 추가

---
+++ src/dummy.js
@@ -0,0 +1,54 @@
+export const sales = [
+    {
+        "saleId": 1,
+        "registeredDate": "2018-03-01",
+        "saleStatus": "SALE",
+        "name": "부자아파트",
+        "dong": 105,
+        "hosu": 304,
+        "pyung": 32,
+        "direction": "남",
+        "type": "JEONSE",
+        "deposit": null,
+        "rent": 12
+    },
+    {
+        "saleId": 2,
+        "registeredDate": "2018-03-02",
+        "saleStatus": "SALE",
+        "name": "최고부자아파트",
+        "dong": 101,
+        "hosu": 203,
+        "pyung": 54,
+        "direction": "동",
+        "type": "TRADE",
+        "deposit": null,
+        "rent": 11
+    },
+    {
+        "saleId": 3,
+        "registeredDate": "2018-03-03",
+        "saleStatus": "SALE",
+        "name": "거지아파트",
+        "dong": 203,
+        "hosu": 101,
+        "pyung": 23,
+        "direction": "동",
+        "type": "TRADE",
+        "deposit": null,
+        "rent": 30
+    },
+    {
+        "saleId": 4,
+        "registeredDate": "2018-03-01",
+        "saleStatus": "SOLDOUT",
+        "name": "남의아파트",
+        "dong": 301,
+        "hosu": 204,
+        "pyung": 32,
+        "direction": "남",
+        "type": "TRADE",
+        "deposit": null,
+        "rent": 28
+    },
+];
